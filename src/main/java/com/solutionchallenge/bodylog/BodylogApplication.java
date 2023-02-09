51d61c101980572ef08bc332121d695dc7d6dca8
김자경 <rlawkrud15@gmail.com>
2023-02-09 20:10:14 +0900

    feat : 초기 설정

---
+++ src/main/java/com/solutionchallenge/bodylog/BodylogApplication.java
@@ -0,0 +1,13 @@
+package com.solutionchallenge.bodylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodylogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BodylogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/solutionchallenge/bodylog/BodylogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solutionchallenge.bodylog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodylogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
