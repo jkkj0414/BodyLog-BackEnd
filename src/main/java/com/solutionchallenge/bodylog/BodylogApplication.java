b716d7a49bc9b2284dfc964a3ec7822429fc0a9f
tndusy27 <tndusy527@gmail.com>
2023-02-27 14:33:54 +0900

    cors 오류수정

--- src/main/java/com/solutionchallenge/bodylog/BodylogApplication.java
+++ src/main/java/com/solutionchallenge/bodylog/BodylogApplication.java
@@ -2,7 +2,10 @@ package com.solutionchallenge.bodylog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -11,5 +14,14 @@ public class BodylogApplication {
     public static void main(String[] args) {
         SpringApplication.run(BodylogApplication.class, args);
     }
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*");
+            }
+        };
+    }
 }
