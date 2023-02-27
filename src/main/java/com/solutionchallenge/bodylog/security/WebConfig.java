17439d4c401bff36498e21ae95b8262ef4d69195
김자경 <rlawkrud15@gmail.com>
2023-02-27 15:11:19 +0900

    fix : CORS 오류 해결

---
+++ src/main/java/com/solutionchallenge/bodylog/security/WebConfig.java
@@ -0,0 +1,30 @@
+package com.solutionchallenge.bodylog.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:8080",
+                        "http://localhost:8443",
+                        "http://bodylog1.duckdns.org"
+                )
+                .allowedMethods(HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.OPTIONS.name()
+                )
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
