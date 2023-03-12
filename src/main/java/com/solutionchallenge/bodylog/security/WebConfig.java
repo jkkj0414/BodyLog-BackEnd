c417fa81fabed2aa5cc082e359726fcb3971e542
tndusy27 <tndusy527@gmail.com>
2023-03-13 01:19:42 +0900

    fix:cors 오류

--- src/main/java/com/solutionchallenge/bodylog/security/WebConfig.java
+++ src/main/java/com/solutionchallenge/bodylog/security/WebConfig.java
@@ -9,22 +9,36 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:8080",
-                        "http://localhost:8443",
-                        "https://bodylog1.duckdns.org"
-                )
-                .allowedMethods(HttpMethod.GET.name(),
-                        HttpMethod.POST.name(),
-                        HttpMethod.PATCH.name(),
-                        HttpMethod.PUT.name(),
-                        HttpMethod.DELETE.name(),
-                        HttpMethod.OPTIONS.name()
-                )
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins(
+//                        "http://localhost:8080",
+//                        "http://localhost:8443",
+//                        "https://bodylog1.duckdns.org"
+//                )
+//                .allowedMethods(HttpMethod.GET.name(),
+//                        HttpMethod.POST.name(),
+//                        HttpMethod.PATCH.name(),
+//                        HttpMethod.PUT.name(),
+//                        HttpMethod.DELETE.name(),
+//                        HttpMethod.OPTIONS.name()
+//                )
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
+//    }
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+             registry.addMapping("/**")
+              .allowedOrigins("*")
+                .allowedMethods(
+                    HttpMethod.GET.name(),
+                    HttpMethod.HEAD.name(),
+                    HttpMethod.POST.name(),
+                    HttpMethod.PUT.name(),
+                    HttpMethod.DELETE.name()
+            );
+}
+
+
 }
