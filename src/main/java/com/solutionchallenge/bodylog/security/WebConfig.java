2bb8f092abd4ff6c0e3b8ed17b6528ff46a6fa3e
tndusy27 <tndusy527@gmail.com>
2023-03-13 10:49:59 +0900

    cors 수정중

--- src/main/java/com/solutionchallenge/bodylog/security/WebConfig.java
+++ src/main/java/com/solutionchallenge/bodylog/security/WebConfig.java
@@ -35,6 +35,7 @@ public class WebConfig implements WebMvcConfigurer {
                     HttpMethod.GET.name(),
                     HttpMethod.HEAD.name(),
                     HttpMethod.POST.name(),
+                    HttpMethod.PATCH.name(),
                     HttpMethod.PUT.name(),
                     HttpMethod.DELETE.name()
             );
