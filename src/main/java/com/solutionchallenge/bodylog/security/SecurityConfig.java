5a82bea1860a6e1ab550f543ee2a7dfe9b2a09f5
tndusy27 <tndusy527@gmail.com>
2023-03-15 16:23:09 +0900

    fix:cors오류수정

--- src/main/java/com/solutionchallenge/bodylog/security/SecurityConfig.java
+++ src/main/java/com/solutionchallenge/bodylog/security/SecurityConfig.java
@@ -11,6 +11,12 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -19,6 +25,7 @@ public class SecurityConfig {
 
     private final TokenProvider tokenProvider;
     private final RedisTemplate redisTemplate;
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -31,21 +38,34 @@ public class SecurityConfig {
                 .antMatchers("/login", "/join","/").permitAll()
                 .antMatchers("/log-out").authenticated()
 //               .antMatchers("/user/**","/@**/add","/**/**/update", "/api/**","/").hasAnyRole("USER", "ADMIN")
-               .antMatchers("/user/**","/**/add","/**/**/update","/**/**/delete", "/").hasAnyRole("USER", "ADMIN")
-
+                .antMatchers("/user/**","/**/add","/**/**/update","/**/**/delete", "/api/**/**/**","/").hasAnyRole("USER", "ADMIN")
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .addFilterBefore(new JwtAuthFilter(tokenProvider, redisTemplate), UsernamePasswordAuthenticationFilter.class)
-                .cors();
+                .cors().configurationSource(configurationSource());
         return http.build();
     }
+        @Bean
+        public CorsConfigurationSource configurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
 
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:3000"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setExposedHeaders(List.of("Access-Control-Allow-Credentials", "Authorization", "Set-Cookie"));
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+}
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
-
 
 }
 
--- src/main/java/com/solutionchallenge/bodylog/security/WebConfig.java
+++ src/main/java/com/solutionchallenge/bodylog/security/WebConfig.java
@@ -3,8 +3,6 @@ package com.solutionchallenge.bodylog.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
